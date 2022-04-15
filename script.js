d0a69e3b2c0ecf7431d14a3bca68d01370266047
vasilievanton <vasilievanton36@gmail.com>
2022-04-15 16:20:22 +0300

    done_v_5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('.header__menu-icon').click(function(event) {
-        $('.header__menu-icon,.menu').toggleClass('active');
-    });
-});
+// $(document).ready(function() {
+//     $('.header__menu-icon').click(function(event) {
+//         $('.header__menu-icon,.menu').toggleClass('active');
+//     });
+// });
